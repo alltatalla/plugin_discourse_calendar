b9c6dd5028d1f831f6bee126f92afab659db49a9
kamratekid <kamratekid@users.noreply.github.com>
2015-08-04 18:52:46 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,12 +1,12 @@
-# name: Google Calendar Onebox
+# name: fruux calendar Onebox
 # version: 0.1
-# authors: Daniel Fernández
+# authors: kamratekid, Daniel Fernández
 
-# freedom patch Google Calendar Onebox
-class Onebox::Engine::CalendarOnebox
+# freedom patch fruux calendar Onebox
+class Onebox::Engine::fruuxCalendarOnebox
   include Onebox::Engine
 
-  matches_regexp(/^https?:\/\/(?:www\.)?(?:m\.)?(?:google\.com\/calendar)\/.+$/)
+  matches_regexp(/^https?:\/\/(?:www\.)?(?:m\.)?(?:fruux\.com\/calendars)\/.+$/)
 
   def to_html
     "<iframe src='#{@url}' style='border: 0' width='800' height='600' frameborder='0' scrolling='no'></iframe>"
